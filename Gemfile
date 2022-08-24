f6fa21ef56647ce68538b56d7851a781cf9edd35
Emre Ebeturk <emreebeturk@gmail.com>
2022-08-24 14:45:18 +0200

    Only partially successful attempt at cloudinary...

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby "3.1.2"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 
+gem "cloudinary"
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
@@ -53,13 +55,13 @@ gem "simple_form", github: "heartcombo/simple_form"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
-  gem "dotenv-rails"
 
 end
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+  gem "dotenv-rails"
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
@@ -78,6 +80,3 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
-
-gem "dotenv-rails", groups: [:development, :test]
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,9 +6,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-# puts "Cleaning database..."
-# Dragon.destroy_all
+file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Smaug_par_David_Demaret.jpg/290px-Smaug_par_David_Demaret.jpg')
+puts "Cleaning database..."
+Dragon.destroy_all
 require "faker"
 
 puts "Creating dragons..."
@@ -17,16 +18,18 @@ puts "Creating dragons..."
   email: Faker::Internet.email,
   password: 123456
 )
-  10.times do |n|
-      Dragon.create!(
+  10.times do |i|
+      dragon = Dragon.create!(
       name: Faker::TvShows::GameOfThrones.dragon.capitalize,
       power: ["Enhanced Senses", "Enhanced Speed", "Flight", "Pyrokinesis", "Shapeshifting", "Combat", "Bake Cookies", "Drink Fire", "Super Healer", "Super Nanny", "Plant Propagator", "Super Fire Extinguisher", "Super Nice Taco Maker", "Super Debugger", "Super Drinker" ].shuffle.first,
-  age: rand(1..1500),
+      age: rand(1..1500),
       level: rand(1..10),
       price: rand(5..500),
       user: user
-  )
-  end
+      )
+
+    end
+
 end
 
 
