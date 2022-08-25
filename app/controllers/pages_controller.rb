65c8b73ccca4551184ed562842588b18d541f08e
Emre Ebeturk <emreebeturk@gmail.com>
2022-08-25 13:06:17 +0200

    seed with cloudinary photos accomplished.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,7 @@
 class PagesController < ApplicationController
   def home
-      params[:query].present?
-        sql = "name ILIKE :query OR power ILIKE :query"
-        @dragons = Dragon.where(sql, query: "%#{params[:query]}%")
+      sql = "name @@ :query OR power @@ :query"
+      @dragons = Dragon.where(sql, query: "%#{params[:query]}%")
 
     end
     # @dragons = Dragon.where(active: true).limit(3)
