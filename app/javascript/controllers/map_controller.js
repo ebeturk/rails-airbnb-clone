e4bde9b3bd114b32ad9ed4e55632790f93993cf2
Sideris Bassi <raphaellasidb@gmail.com>
2022-08-26 11:31:38 +0200

    markes incomplete avatar and show

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/light-v10"
     })
 // <<<<<<< HEAD
 //     #addMarkersToMap() {
@@ -45,6 +45,22 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
->>>>>>> master
+// >>>>>>> master
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const customMarker = document.createElement('div')
+      customMarker.style.height = "30px"
+      customMarker.style.width = "30px"
+      customMarker.style.backgroundImage = 'url('${ marker.image_url }')'
+
+
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window) // Add this
+      new mapboxgl.Marker( element: customMarker)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup) // Add this
+        .addTo(this.map)
+    });
   }
 }
