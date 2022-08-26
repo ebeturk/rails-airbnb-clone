a326f62d3992a18a0b152a7ebf8434c6190a3e5a
Brane Bencic <brane.bencic@gmail.com>
2022-08-26 11:58:34 +0200

    fixing map stimulus controller

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -10,39 +10,14 @@ export default class extends Controller {
   connect() {
     console.log(this.apiKeyValue)
     mapboxgl.accessToken = this.apiKeyValue
-<<<<<<< HEAD
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
-=======
->>>>>>> master
 
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/light-v10"
     })
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    // #addMarkersToMap() {
-    //   this.markersValue.forEach((marker) => {
-    //     new mapboxgl.Marker()
-    //       .setLngLat([ marker.lng, marker.lat ])
-    //       .addTo(this.map)
-    //   })
-    // }
-
-    // #fitMapToMarkers() {
-    //   const bounds = new mapboxgl.LngLatBounds()
-    //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-    // }
-
-=======
->>>>>>> master
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
->>>>>>> master
   }
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
@@ -56,14 +31,6 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> master
-=======
-// >>>>>>> master
->>>>>>> master
   }
 
   #addMarkersToMap() {
