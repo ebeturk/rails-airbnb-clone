f02f52bdfa034dccf6ab1d16a5c844a5ba6964aa
Emre Ebeturk <emreebeturk@gmail.com>
2022-08-26 09:03:40 +0200

    commented out map marker part

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -16,19 +16,19 @@ export default class extends Controller {
       style: "mapbox://styles/mapbox/streets-v10"
     })
 
-    #addMarkersToMap() {
-      this.markersValue.forEach((marker) => {
-        new mapboxgl.Marker()
-          .setLngLat([ marker.lng, marker.lat ])
-          .addTo(this.map)
-      })
-    }
+    // #addMarkersToMap() {
+    //   this.markersValue.forEach((marker) => {
+    //     new mapboxgl.Marker()
+    //       .setLngLat([ marker.lng, marker.lat ])
+    //       .addTo(this.map)
+    //   })
+    // }
 
-    #fitMapToMarkers() {
-      const bounds = new mapboxgl.LngLatBounds()
-      this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-    }
+    // #fitMapToMarkers() {
+    //   const bounds = new mapboxgl.LngLatBounds()
+    //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    // }
 
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
