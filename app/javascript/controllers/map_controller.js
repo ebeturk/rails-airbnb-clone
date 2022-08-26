eefdec2a0b509177e9c8c2e2fa2378d070d7c495
Sideris Bassi <raphaellasidb@gmail.com>
2022-08-26 12:24:00 +0200

    markers tags og missing the info

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -19,31 +19,24 @@ export default class extends Controller {
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
   }
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(this.map)
-    })
-  }
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-// >>>>>>> master
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const customMarker = document.createElement('div')
+      customMarker.className = "marker"
       customMarker.style.height = "30px"
       customMarker.style.width = "30px"
-      customMarker.style.backgroundImage = 'url('${ marker.image_url }')'
-
+      customMarker.style.backgroundSize = "cover"
+      customMarker.style.backgroundImage = `url('${ marker.image_url }')`
 
       const popup = new mapboxgl.Popup().setHTML(marker.info_window) // Add this
-      new mapboxgl.Marker( element: customMarker)
+      new mapboxgl.Marker(customMarker)
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup) // Add this
         .addTo(this.map)
